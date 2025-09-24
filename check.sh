b206678ddc1a50f6feb6adcea504ac529a14d831
gaidardzhiev <1v4n@protonmail.com>
2025-09-24 10:39:09 +0000

    Wed Sep 24 10:39:09 AM UTC 2025

---
+++ check.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+G='\033[0;32m'
+R='\033[0;31m'
+N='\033[0m'
+
+ARCH=$(uname -m)
+
+[ ! "$ARCH" = "x86_64" ] && exit 1
+
+[ ! -f linker ] && make
+
+[ ! -f test.o ] && make test
+
+fprint() {
+	 printf "[%s] Test: %-20s Result: %b\n" "$(date '+%Y-%m-%d %H:%M:%S')" "$1" "$2"
+}
+
+fcheck() {	
+	capture=$(./out.elf)
+	filtered=$(printf "%s" "$capture" | tr -cd '>>> !!! payload executed !!! <<<')
+	expected=">>> !!! payload executed !!! <<<"
+	[ "$filtered" = "$expected" ] && {
+		fprint "Injection Test" "${G}PASSED${N}";
+		return 0;
+	} || {
+		fprint "Injection Test" "${R}FAILED${N}";
+		return 	32;
+	}
+}
+
+{ fcheck; return="$?"; } || exit 1
+
+[ "$return" -eq 0 ] 2>/dev/null || printf "%s\n" "$return"
